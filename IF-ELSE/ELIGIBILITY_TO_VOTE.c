1eec2050905905e8566e7aa044d4068d13502867
P-BARTHWAL <priyanshibarthwal06.06.06@gmail.com>
2026-01-12 16:06:35 +0530

    add new program

---
+++ IF-ELSE/ELIGIBILITY_TO_VOTE.c
@@ -0,0 +1,17 @@
+/*15. ELIGIBILITY TO VOTE */
+#include <stdio.h>
+int main() {
+    int age;
+    printf (" Enter your age : ");
+    scanf ("%d", &age);
+    if (age<=0)
+    {printf (" INVALID AGE ");}
+    else
+    {
+         if(age >= 18)
+         {printf (" You are eligible to VOTE ");}
+         else
+         { printf ( "You are not eligible to vote ");}
+    }
+    return 0;
+}
