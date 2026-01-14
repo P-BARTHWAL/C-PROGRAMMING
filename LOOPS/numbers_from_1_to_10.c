daae042f9600b21df67a69a735ec8aa8aa4d9ce6
P-BARTHWAL <priyanshibarthwal06.06.06@gmail.com>
2026-01-14 16:00:13 +0530

    add new program

---
+++ LOOPS/numbers_from_1_to_10.c
@@ -0,0 +1,14 @@
+//Program to print numbers from 1 to 10
+# include<stdio.h>
+int main()
+{
+   int n;
+    n=1;
+    printf("Numbers from 1 to 10 are; \n");
+    while (n<=10)
+    { printf("%d \n",n);
+    n=n+1;}
+    return 0;
+}
+
+
