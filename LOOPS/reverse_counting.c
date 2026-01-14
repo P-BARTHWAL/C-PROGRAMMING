47c7d6d008cdb939ba96663453005654b37c1294
P-BARTHWAL <priyanshibarthwal06.06.06@gmail.com>
2026-01-14 16:10:32 +0530

    add new program

---
+++ LOOPS/reverse_counting.c
@@ -0,0 +1,12 @@
+//Program to print numbers from 10 to 1
+# include<stdio.h>
+int main()
+{
+   int n;
+    n=10;
+    printf("Numbers from 10 to 1 are; \n");
+    while (n>=1)
+    { printf("%d \n",n);
+    n=n-1;}
+    return 0;
+}
