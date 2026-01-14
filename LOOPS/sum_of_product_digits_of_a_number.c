2fcef62e3652fe02ac70ed5794577c0d86e69194
P-BARTHWAL <priyanshibarthwal06.06.06@gmail.com>
2026-01-14 17:26:15 +0530

    add new program

---
+++ LOOPS/sum_of_product_digits_of_a_number.c
@@ -0,0 +1,22 @@
+// Program to find the product of digits of a number
+#include <stdio.h>
+int main()
+{
+long int num,orig_num, product=1;
+int digit;
+printf("Enter a number: ");
+scanf("%ld", &num);
+orig_num = num;
+if (num==0)
+{product=0; }
+else if (num<0)
+num= -orig_num;
+while (num!= 0)
+{ digit = num % 10;
+  product = product*digit;
+  num = num/ 10; }
+
+printf("Product of digits of number %ld = %ld", orig_num,product);
+return 0;
+}
+
