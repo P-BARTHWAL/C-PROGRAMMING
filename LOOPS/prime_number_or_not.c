d83655df8efcb85adf4bf5097a55f388eec51123
P-BARTHWAL <priyanshibarthwal06.06.06@gmail.com>
2026-01-14 19:05:09 +0530

    add new program

---
+++ LOOPS/prime_number_or_not.c
@@ -0,0 +1,21 @@
+
+//Program to check whether a number is prime or not.
+#include <stdio.h>
+int main()
+{
+int num,i,flag=0;
+printf("Enter a number:");
+scanf("%d",&num);
+for(i=1;i<=num;i++)
+{if (num%i==0)
+flag=flag+1;
+}
+if (flag==2)
+printf("%d is a prime number",num);
+else
+printf("%d is not a prime number",num);
+return 0;
+}
+
+
+
