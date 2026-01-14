cfc1ca72d0e3bbb3a7faeaf03e81c13688cfa13c
P-BARTHWAL <priyanshibarthwal06.06.06@gmail.com>
2026-01-14 16:30:12 +0530

    add new program

---
+++ LOOPS/only_even_numbers_from_100_to_1.c
@@ -0,0 +1,12 @@
+// Program to print even numbers from 1 to 100
+# include <stdio.h>
+int main()
+{
+int i;
+printf("Even numbers from 1 to 100 are; \n");
+for (i=1;i<=100;i++)
+{ if (i%2!=0)
+  printf("%d,",i);
+}
+return 0;
+}
