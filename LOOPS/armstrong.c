b273a304ffc62febe560070aa4febdffd381ad50
P-BARTHWAL <priyanshibarthwal06.06.06@gmail.com>
2026-01-14 17:31:49 +0530

    add new program

---
+++ LOOPS/armstrong.c
@@ -0,0 +1,25 @@
+
+
+
+
+
+//Program to check whether a number is armstrong or not
+#include <stdio.h>
+int main()
+{
+int num,orig_num,digit,a=0 ;
+printf("Enter a number:");
+scanf("%d",&num);
+orig_num = num ;
+while (num>0)
+{ digit= num%10;
+  a= a+(digit*digit*digit) ;
+  num= num/10;
+}
+if (orig_num==a)
+printf("%d is armstrong number",orig_num);
+else
+printf("%d is not armstrong number",orig_num);
+return 0;
+}
+
